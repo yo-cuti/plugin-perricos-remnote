aa06b875e73682ce29634ee907a5c0b39dc1e7ee
capitNsalami <jparagit3@gmail.com>
2025-03-20 19:05:15 +0100

    Añadimos las motivaciones al azar, fallo del commit de antes

---
+++ src/lib/motivacion.tsx
@@ -0,0 +1,21 @@
+import { getRandomInt } from "./utils";
+
+export const motivaciones = [
+  "Estás bien perrona 🔥",
+  "Eres un hueso duro de roer... 🦴",
+  "Dale colega! 💪",
+  "Enseñales quien es el alfa 🐺",
+  "Genial OwO **mueve la colita**",
+  "Toma un perrete de regalo",
+  "Americano Dios bendito...🙏⛪",
+  "Quién está mi chic@ guap@... 😏",
+  "¡Bien hecho!",
+  "Buen trabajo 💯",
+  "Genial ✨",
+  "¡Sigue así! 🤑"
+];
+
+export const motivacionRandom = () => {
+  const idx = getRandomInt(0, motivaciones.length - 1);
+  return motivaciones[idx];
+};
