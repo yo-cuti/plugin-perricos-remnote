ab01273d21be2dc4ec80f47457737ddbf2321089
capitNsalami <jparagit3@gmail.com>
2025-03-20 19:04:10 +0100

    Cambio de estilos Añadimos las motivaciones al azar

--- src/widgets/popupPerro.tsx
+++ src/widgets/popupPerro.tsx
@@ -8,6 +8,7 @@ import {
   } from "@remnote/plugin-sdk";
   import { tomaPerrico } from "../lib/perricos";
   import { useEffect, useState } from "react";
+  import { motivacionRandom } from "../lib/motivacion";
 //   import { randomMotivation } from "../lib/motivation";
   
   function PopupPerro() {
@@ -50,10 +51,9 @@ import {
             }}
             className="cursor-pointer rounded-md border border-solid flex gap-1 justify-center align-middle rn-clr-background-primary rn-clr-content-primary w-fit"
           >
-            <img src={src} className="max-h-[160px] max-w-[200px] w-auto h-auto rounded-l-md" />
+            <img src={src} className="max-h-[160px] max-w-[200px] w-auto h-auto rounded-l-md" style={{ maxHeight: "160px"}}/>
             <div className="flex text-center text-lg pr-2 w-[calc-size(fit-content,_size_+_20px)] items-center">
-              {/* Wow! {seenCards} cards done! {randomMotivation()}! */}
-              <span className="h-fit">Perro guay!</span>
+              <span className="h-fit p-5 min-w-[200px] whitespace-nowrap">¡Has acertado {seenCards} cartas!<br></br>{motivacionRandom()}</span>
             </div>
           </div>
         }
